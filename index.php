ee342398daada44c9faf207a01c070c2b00da0ac
Alexvachack <alexvachack48@gmail.com>
2020-10-18 17:13:24 -0500

    Revert "envio de correos mud-arte.com"
    This reverts commit ee1f669f397d13d68cc616f628ab821fa1172a20.

--- index.php
+++ index.php
@@ -8,36 +8,19 @@ use PHPMailer\PHPMailer\Exception;
 
 $oMail = new PHPMailer();
 $oMail->isSMTP();
-$oMail->Host = "mail.mud-arte.com";
+$oMail->Host = "dominio.com";
 $oMail->Port = "26";
 $oMail->SMTPSecure = "tls";
 $oMail->SMTPAuth = true;
-$oMail->Username = "contacto2@mud-arte.com";
-$oMail->Password = "mud-arte0101*";
-$oMail->setFrom("contacto2@mud-arte.com", "contacto2@mud-arte.com");
-//$oMail->addAddress("jbrown@brandsolutions.com.mx", "jbrown@brandsolutions.com.mx");
-$oMail->addAddress("heyyouvc@gmail.com", "heyyouvc@gmail.com");
-//$oMail->addAddress("contactomudarte@gmail.com", "contactomudarte@gmail.com");
-$oMail->Subject = "Nueva cotizacion express de Heyyouvc";
+$oMail->Username = "contacto@dominio.com";
+$oMail->Password = "*";
+$oMail->setFrom("contacto@dominio.com", "contacto@dominio.com");
+$oMail->addAddress("destino@gmail.com", "Destino@gmail.com");
+$oMail->Subject = "Nuevo contacto";
 $oMail->msgHTML(
     "
-<div style='align-content: center'>
-<img src='http://mud-arte.com/wp-content/uploads/2019/10/logo.png' alt='MUD-ARTE' style='text-align: center'/>
-<br>
-<img src='http://mud-arte.com/calculadoras/mudanzaCajasFelices.jpg' alt='MUD-ARTE' style='text-align: center'/>
-<br>
-</div>
-<p>Hola jbrown@brandsolutions.com.mx, tienes una nueva cotización local y esta es la información:</p>
-<p>Nombre: nombre</p>
-<p>Correo: correo</p>
-<p>Teléfono:   telefono</p>
-<p>Origen: origen</p>
-<p>Destino:    destino</p>
-<p>Transporte: transporte</p>
-<p>Personal:   personal</p>
-<p>Cotización: pesos</p>
- <br>
- <br>"
+
+<p>Hola tienes un nuevo contacto en tu dominio.com</p>"
 );
 if (!$oMail->send())
     echo $oMail->ErrorInfo;
